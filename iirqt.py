b3fcc3fa02f287da2c466fda3bbf309319768ab9
Andrew Seitz <acseitz.as@gmail.com>
2015-04-16 22:51:18 -0400

    FIxed a bug where the selected channel and server were always the LAST selected of each

--- iirqt.py
+++ iirqt.py
@@ -1,7 +1,11 @@
 import sys
+import sip
 
+sip.setapi('QString', 2)
+sip.setapi('QVariant', 2)
+
+from PyQt4 import QtCore, QtGui
 from qtreactor import pyqt4reactor
-from PyQt4 import QtGui
 from twisted.python import log
 
 
@@ -125,22 +129,6 @@ class MainWindow(QtGui.QMainWindow):
         connectAction.triggered.connect(self.connectWindow)
         serverMenu.addAction(connectAction)
 
-        # # [ [server name] [nickname] (connect) ]
-        # serverLine = QtGui.QHBoxLayout()
-        #
-        # serverLine.addWidget(QtGui.QLabel('Server'))
-        # self.serverName = QtGui.QLineEdit('irc.freenode.net')
-        # serverLine.addWidget(self.serverName)
-        #
-        # serverLine.addWidget(QtGui.QLabel('Nickname'))
-        # self.nickName = QtGui.QLineEdit('twisted_toast2')
-        # serverLine.addWidget(self.nickName)
-        #
-        # self.connectButton = QtGui.QPushButton('Connect')
-        # serverLine.addWidget(self.connectButton)
-        # self.connectButton.clicked.connect(self.IRCConnectServer)
-
-
         # [ [tree channel list] [                    chat window                   ]
         """
         Set up the channel tree widget.
@@ -159,19 +147,23 @@ class MainWindow(QtGui.QMainWindow):
         """
         self.bufferDict = {}
 
-        chatLine = QtGui.QHBoxLayout()
 
         # Channel Tree
         self.channelTree = QtGui.QTreeWidget()
         self.channelTree.setColumnCount(1)
         self.channelTree.setFixedWidth(200)
         self.channelTree.setHeaderLabel('Channels')
-        self.channelTree.itemClicked.connect(self.changeView)
+        self.channelTree.currentItemChanged.connect(self.changeView)
+
+        # Label to display current nicknam
+        self.nickLabel = QtGui.QLabel(' ')
+        self.nickLabel.setAlignment(QtCore.Qt.AlignRight)
+        # self.nickLabel.set
 
         # Chat window
         self.chatWindow = QtGui.QStackedWidget()
         self.coreList = QtGui.QListWidget()
-        self.coreList.addItem('Test')
+        self.coreList.addItem('Welcome to IIRC')
 
         self.chatWindow.addWidget(self.coreList)
 
@@ -180,30 +172,35 @@ class MainWindow(QtGui.QMainWindow):
         self.entry.returnPressed.connect(self.handle_line)
 
         # Add the core buffer as the first chat view
-        self.addServerToTree('IIRC Core', 'corebuffer')
-        self.bufferDict['IIRC Core'] = {'index': 1}
+        self.addServerToTree('IIRC-Core', 'corebuffer')
+        # self.bufferDict['IIRC Core'] = {'nick': 'Core Buffer', 'buffer': self.coreList}
+        self.nickLabel.setText('Core Buffer')
 
-        chatLine.addWidget(self.channelTree)
-        chatLine.addWidget(self.chatWindow)
 
-        entryLine = QtGui.QHBoxLayout()
+        chatAndEntry = QtGui.QVBoxLayout()
+        chatAndEntry.addWidget(self.chatWindow)
+        chatAndEntry.addWidget(self.entry)
 
-        self.channelName = QtGui.QLineEdit('#secretfun')
-        self.channelName.setFixedWidth(150)
-        entryLine.addWidget(QtGui.QLabel('Channel'))
-        entryLine.addWidget(self.channelName)
-        entryLine.addWidget(QtGui.QLabel('<<'))
-        entryLine.addWidget(self.entry)
+        treeAndLabel = QtGui.QVBoxLayout()
+        treeAndLabel.addWidget(self.channelTree)
+        treeAndLabel.addWidget(self.nickLabel)
+
+        # chatLine = QtGui.QHBoxLayout()
+        # chatLine.addWidget(self.channelTree)
+        # chatLine.addWidget(self.chatWindow)
+
+        # entryLine = QtGui.QHBoxLayout()
+        #
+        # entryLine.addWidget(QtGui.QLabel('Channel'))
+        # entryLine.addWidget(QtGui.QLabel('<<'))
+        # entryLine.addWidget(self.entry)
+
+        hLayout = QtGui.QHBoxLayout()
+        hLayout.addLayout(treeAndLabel)
+        hLayout.addLayout(chatAndEntry)
 
         mainLayout = QtGui.QWidget(self)
-
-        vbox = QtGui.QVBoxLayout()
-        # vbox.addLayout(serverLine)
-        vbox.addLayout(chatLine)
-        # vbox.addWidget(self.view)
-        vbox.addLayout(entryLine)
-
-        mainLayout.setLayout(vbox)
+        mainLayout.setLayout(hLayout)
 
         self.statusBar().showMessage('Ready to Connect')
         self.setCentralWidget(mainLayout)
@@ -214,6 +211,10 @@ class MainWindow(QtGui.QMainWindow):
         self.protocol = None
         """End layout setup"""
 
+
+        self.entry.insert('/connect irc.freenode.net twstd')
+        # self.IRCConnectServer('irc.freenode.net', 'twstd')
+
         self.relayFactory = RelayFactory(window=self)
         log.msg('relayFactory.window: ', self.relayFactory.window)
 
@@ -222,12 +223,6 @@ class MainWindow(QtGui.QMainWindow):
         reactor.connectTCP('localhost', 9993, self.relayFactory)
         reactor.run()
 
-        if DEBUG is True:
-            self.IRCConnectServer()
-            self.entry.insert('/join #secretfun')
-
-            # def connect_irc_server(self):
-            # self.
 
     def connectWindow(self):
         self.cw = connectDialog()
@@ -249,11 +244,14 @@ class MainWindow(QtGui.QMainWindow):
         # Count always one more than highest index
         index = self.chatWindow.count()
         newBuffer = QtGui.QListWidget()
-        self.bufferDict[identifier] = {'index': index, 'buffer': newBuffer, 'nick': str(nickname)}
+        self.bufferDict[identifier] = {'index': index,
+                                       'buffer': newBuffer,
+                                       'nick': str(nickname),
+                                       'treetab': newServer}
 
         # Create the chat view
         self.chatWindow.addWidget(newBuffer)
-        self.chatWindow.setCurrentIndex(index)
+        self.chatWindow.setCurrentWidget(newBuffer)
 
     def clearTreeSelections(self):
         selections = self.channelTree.selectedItems()
@@ -272,7 +270,9 @@ class MainWindow(QtGui.QMainWindow):
         identifier = str(server.text(0)) + str(channelName)
         index = self.chatWindow.count()
         newBuffer = QtGui.QListWidget()
-        self.bufferDict[identifier] = {'index': index, 'buffer': newBuffer}
+        self.bufferDict[identifier] = {'index': index,
+                                       'buffer': newBuffer,
+                                       'treetab': newChannel}
         self.chatWindow.addWidget(newBuffer)
 
         # Expand the server tree
@@ -307,7 +307,6 @@ class MainWindow(QtGui.QMainWindow):
 
         log.msg('IRCSendLine server: ', server, '  channel: ', channel)
 
-        # TODO: set up an array to hold servers and channels, use nickname from there
         line = 'sendLine {0} {1} {2} {3}'.format(server, channel, nickname, message)
         lineToShow = '{0} {1} {2} {3}'.format(server, channel, nickname, message)
         self.show(lineToShow, clear=True, bottom=True)
@@ -354,25 +353,34 @@ class MainWindow(QtGui.QMainWindow):
         if bottom:
             buffer.scrollToBottom()
 
-    def getSelectedServer(self):
+    def getSelectedServer(self, force=None):
         # Returns the currently selected ITEM in the channel tree
-        item = self.channelTree.selectedItems()[0]
+        # Sometimes does not behave right.  force lets me pass in a specific item instead
+        if force is None:
+            item = self.channelTree.selectedItems()[0]
+            log.msg('selectedItems: ', self.channelTree.selectedItems()[0].text(0))
+        else:
+            item = force
+        # item = self.channelTree.currentItem()
         log.msg('Query for server: ', item.text(0), item.parent().text(0) if item.parent() is not None else 'no parent')
         if item.parent() is None:
             return item
         else:
             return item.parent()
 
-    def getSelectedChannel(self):
+    def getSelectedChannel(self, force=None):
         # TODO: Check to make sure it's actually a channel
-        item = self.channelTree.selectedItems()[0]
+        if force is None:
+            item = self.channelTree.selectedItems()[0]
+        else:
+            item = force
         return item
 
     def handle_line(self):
         line = self.entry.text()
         log.msg('Handling line: ', line)
 
-        if line.startsWith('/join'):
+        if line.startswith('/join'):
             # Join a channel
             # > join <server> <channel>
             channel = line.split(' ', 1)[1]
@@ -388,7 +396,7 @@ class MainWindow(QtGui.QMainWindow):
             # self.IRCConnectChannel(self.getSelectedServer())
 
         # elif cmd[0] == '/connect':
-        elif line.startsWith('/connect'):
+        elif line.startswith('/connect'):
             # Connect to a server
             # > connect <server> <nickname>
             arg = line.split(' ', 2)
@@ -401,17 +409,20 @@ class MainWindow(QtGui.QMainWindow):
             # > sendLine <server> <channel> <message>
             self.IRCSendLine(line)
 
-    def changeView(self, item, index):
-        log.msg('changeView triggered, item: ', item, '  index: ', index)
-        server = self.getSelectedServer()
-        channel = self.getSelectedChannel()
-        identifier = str(server.text(0)) + str(channel.text(0))
+    def changeView(self, current, previous):
+        log.msg('changeView triggered, item: ', current.text(0), '  old: ', previous.text(0))
+        server = self.getSelectedServer(force=current)
+        channel = self.getSelectedChannel(force=current)
+        log.msg('changeView server: ', server.text(0), '  channel: ', channel.text(0))
         # A server buffer will have identical server and channel
         if server == channel:
             buffer = self.bufferDict[str(server.text(0))]['buffer']
         else:
+            identifier = str(server.text(0)) + str(channel.text(0))
             buffer = self.bufferDict[identifier]['buffer']
         self.chatWindow.setCurrentWidget(buffer)
+        current.setBackgroundColor(0, QtGui.QColor('#FFEFD5'))
+        previous.setBackgroundColor(0, QtGui.QColor('#FFFFFF'))
 
 
 if __name__ == '__main__':
